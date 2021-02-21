a45f091583d9c54d59b2a3f3001dff08ce2c4289
Mattk <email@mattkirkland.co.uk>
2021-02-21 00:00:02 +0000

    checking SQL value conversions

--- bin/user/belchertown-dev.py
+++ bin/user/belchertown-dev.py
@@ -3789,12 +3789,12 @@ class HighchartsJsonGenerator(weewx.reportengine.ReportGenerator):
             query = archive.genSql(sql_lookup)
             for row in query:
                 xAxis_labels.append(row[0])
-                logerr("before conversion: ")
-                logerr(str(row[1])[1:-1]  )
+                logerr("before conversion: ", observation)
+                logerr(str(row[1])[:])
                 row_tuple = (row[1], obs_unit_from_target_unit, obs_group)
                 row_converted = self.converter.convert(row_tuple)
                 logerr("after conversion: ")
-                logerr(str(row_converted[0])[1:-1]  )
+                logerr(str(row_converted[0])[:])
                 obsvalues.append(row_converted[0])
 
             # If the values are to be mirrored, we need to make them negative
